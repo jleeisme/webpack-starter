85922a84daa6f2a9550fce88565198a2b548d0fc
Jason Lee <jleeisme@gmail.com>
2016-12-06 15:58:34 -0800

    added hello.js
    runs hello world now

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-require('./logger');
+import React from "react";
+import ReactDOM from "react-dom";
+import Hello from "./hello";
 
-document.write('ffndfogando');
-console.log('app loaded');
+ReactDOM.render(
+  <Hello name="World" />,
+  document.body//rendering components directly into the body is discouraged
+);
